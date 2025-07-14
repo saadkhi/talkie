1865b98badf37dd0f1b949b52764c5c02b400a75
saadkhi <saadatherali1256@gmail.com>
2025-07-14 16:37:52 +0500

    hello new files

---
+++ talkie/talkie/views.py
@@ -0,0 +1,13 @@
+# from django.http import HttpResponse
+# from django.shortcuts import render
+
+# def home(request):
+#     return render(request, 'website/index.html')
+
+# def about(request):
+#     # return HttpResponse("About Talkie: A platform for conversations.")
+#     return render(request, 'website/about.html')
+
+# def contact(request):
+#     # return HttpResponse("Contact us")
+#     return render(request, 'website/contact.html')
